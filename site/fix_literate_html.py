d714735dfdd99d031f319a0bfab2454967807013
Zachary Feng <zachary.feng@hotmail.com>
2026-09-09 14:26:27 +0100

    Fixed merge conflict

--- site/fix_literate_html.py
+++ site/fix_literate_html.py
@@ -207,6 +207,9 @@ def main():
     # Written last so that the redirect page is not treated as a module page.
     create_root_index(literate_dir)
 
+    # Written last so that the redirect page is not treated as a module page.
+    create_root_index(literate_dir)
+
 
 if __name__ == '__main__':
     main()
